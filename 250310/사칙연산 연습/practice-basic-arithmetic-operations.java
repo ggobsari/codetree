8bdd860f93dd9abf3c951cd9ca8123669ec1e308
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-10 15:17:39 +0900

    Title: 사칙연산 연습, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

--- 250310/사칙연산 연습/practice-basic-arithmetic-operations.java
+++ 250310/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -4,7 +4,7 @@ public class Main {
         int a = 5;
         int b = 30;
         int c = a + b;
-        System.out.println(c /= 8);
-        System.out.println(c %= 8);
+        System.out.println(c / 8);
+        System.out.println(c % 8);
     }
 }
