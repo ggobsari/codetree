8dc1eef39c1df94c78fa189be8b9e3839b829119
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-10 15:08:55 +0900

    Title: 문자 변경하기, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

--- 250310/문자 변경하기/change-charater.java
+++ 250310/문자 변경하기/change-charater.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         String a = "C";
-        String a = "T";
+        a = "T";
         System.out.println(a);
     }
 }
