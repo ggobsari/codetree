341d7284db8fd3df95fd3c12e3dc47302c6757ad
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-11 18:47:13 +0900

    Title: 윤년인가, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 250311/윤년인가/is-leap-year.java
+++ 250311/윤년인가/is-leap-year.java
@@ -10,9 +10,11 @@ public class Main {
         if (y % 4 == 0){
             if (y % 100 == 0 && y % 400 != 0){
                 System.out.print("false");
-                return;
+            }else{
+                System.out.print("true");
             }
-            System.out.print("true");
+        } else{
+            System.out.print("false");
         }
 
     }
