cd549dc4b5c1c221c38787deddf30a5ba67570ac
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-11 18:26:21 +0900

    Title: 점수 비교, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250311/점수 비교/score-comparison.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int aMath = sc.nextInt();
+        int bMath = sc.nextInt();
+
+        int aEnglish = sc.nextInt();
+        int bEnglish = sc.nextInt();
+
+        if (aMath > bMath && aEnglish > bEnglish){
+            System.out.print(1);
+        } else{
+            System.out.print(0);
+        }
+
+
+    }
+}
