558527910338fd88a8d530fa0feebaa305737cdb
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-11 18:15:36 +0900

    Title: M으로 계속 나누기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250311/M으로 계속 나누기/continue-dividing-by-m.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        
+        // Please write your code here.
+        System.out.println(N);
+        while (N != 0){
+            N /= M;
+            System.out.println(N);
+        }
+    }
+}
