fe4e340255824cbbfeb73d4a6215ae8421cf9739
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-11 16:33:40 +0900

    Title: 실수 받아 그대로 출력, Time: 136ms, Memory: 11MB, Status: Passed - CodeTree

--- 250311/실수 받아 그대로 출력/enter-real-value-and-print.java
+++ 250311/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        double n = sc.nextDobule();
+        double n = sc.nextDouble();
 
         System.out.printf("%.2f", n);
     }
