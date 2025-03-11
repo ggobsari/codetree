b62c02d46a634f80caef0deb3187d1226a0dda4f
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-11 21:24:44 +0900

    Title: 배열 채우고 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250311/배열 채우고 출력하기/filling-array-and-print.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        String[] arr = new String[10];
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++){
+            arr[i] = sc.next();
+        }
+
+        String[] reverse = new String[10];
+
+        for (int i = 0; i < 10; i++){
+            reverse[i] = (arr.length - 1) - i;
+        }
+
+        for (int a : reverse){
+            System.out.print(a);
+        }
+    }
+}
