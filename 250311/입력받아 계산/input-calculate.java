e36fb6f90156d5dbd0fed411f4df86f77a4135f2
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-11 16:29:59 +0900

    Title: 입력받아 계산, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

--- 250311/입력받아 계산/input-calculate.java
+++ 250311/입력받아 계산/input-calculate.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        int a = sc.next();
+        int a = sc.nextInt();
 
         System.out.print(a + 2);
     }
