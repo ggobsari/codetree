9b0d9ffd48ac613e251329d949dfe3794cfbd555
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-17 12:17:51 +0900

    Title: A 부터 B 까지, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250317/A 부터 B 까지/a-to-b.java
+++ 250317/A 부터 B 까지/a-to-b.java
@@ -6,22 +6,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int result = a;
 
-        System.out.print(result + " ");
+        System.out.print(a + " ");
 
-        while(true){
-            if (result > b){
+        while(a > 0){
+            if (a > b){
                 break;
             }
 
-            if (result % 2 == 1){
-                result *= 2;
-            } else if (result % 2 == 0){
-                result += 3;
+            if (a % 2 == 1){
+                a *= 2;
+            } else if (a % 2 == 0){
+                a += 3;
             }
 
-            System.out.print(result + " ");
+            System.out.print(a + " ");
         }
     }
 }
