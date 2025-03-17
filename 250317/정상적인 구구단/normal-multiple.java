9e382da37376fe0e7a273033139952dc22132088
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-17 12:55:47 +0900

    Title: 정상적인 구구단, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 250317/정상적인 구구단/normal-multiple.java
+++ 250317/정상적인 구구단/normal-multiple.java
@@ -10,6 +10,9 @@ public class Main {
         for (int i = 1; i <= n; i++){
             for (int j = 1; j <= n; j++){
                 System.out.print(i + " * " + j + " = " + (i * j));
+                if (j < n){
+                    System.out.print(", ");
+                }
             }
             System.out.println();
         }
