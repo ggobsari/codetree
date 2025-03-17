2c8580796f2d933382324ab92f5f091efb881530
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-17 12:23:10 +0900

    Title: A 부터 B 까지, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 250317/A 부터 B 까지/a-to-b.java
+++ 250317/A 부터 B 까지/a-to-b.java
@@ -6,9 +6,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int result = 0;
 
         System.out.print(a + " ");
+        
         for (int i = a; i <= b;) {
             if (i % 2 == 0)
                 i += 3;
