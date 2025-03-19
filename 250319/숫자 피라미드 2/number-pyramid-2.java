ff5a3ab5a174660374f612b4b51d6788389baafb
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-19 13:26:27 +0900

    Title: 숫자 피라미드 2, Time: 172ms, Memory: 11MB, Status: Passed - CodeTree

--- 250319/숫자 피라미드 2/number-pyramid-2.java
+++ 250319/숫자 피라미드 2/number-pyramid-2.java
@@ -7,11 +7,12 @@ public class Main {
 
         int n = sc.nextInt();
 
+        int sum = 1;
+
         for (int i = 1; i <= n; i++){
-            int sum = 1;
             for (int j = 1; j <= i; j++){
                 System.out.print(sum + " ");
-                sum++;
+                sum += 1;
             }
             System.out.println();
         }
