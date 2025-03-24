186e3afa0fb91f6f45d865c96d8d41590e580e50
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-24 14:50:55 +0900

    Title: 특정 원소들의 합, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 250324/특정 원소들의 합/sum-of-specific-elements.java
+++ 250324/특정 원소들의 합/sum-of-specific-elements.java
@@ -10,7 +10,7 @@ public class Main {
         for (int i = 0; i < 4; i++){
             for (int j = 0; j < 4; j++){                
                 int num = sc.nextInt();
-                if (j >= i){
+                if (j <= i){
                     total += num;
                 }
             }
