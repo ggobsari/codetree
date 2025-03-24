bd089e825b65ea9294dbc2fb996ac8e4052d41a4
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-24 14:40:28 +0900

    Title: 배열의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250324/배열의 합/sum-of-array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 4; j++){
+                int total = 0;
+                int num = sc.nextInt();
+                total += num
+                System.out.print(total);
+            }
+            System.out.println();
+        }
+    }
+}
