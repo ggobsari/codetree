ff6a7cd293fb747c689771d97bb9783cf94d99d7
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-24 13:54:18 +0900

    Title: 두 배열의 곱, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250324/두 배열의 곱/multiple-of-two-arrays.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr1 = new int[3][3];
+        int[][] arr2 = new int[3][3];
+
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                int num1 = sc.nextInt();
+                arr1[i][j] = num1;
+            }
+        }
+
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                int num2 = sc.nextInt();
+                arr2[i][j] = num2;
+            }
+        }
+
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                System.out.print((arr1[i][j] * arr2[i][j]) + " ")
+            }
+            System.out.println();
+        }
+    }
+}
