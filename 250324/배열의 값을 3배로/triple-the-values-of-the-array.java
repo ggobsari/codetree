0561612c3063727afbe4753dbeb0fc06c8c07e5d
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-24 13:47:09 +0900

    Title: 배열의 값을 3배로, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250324/배열의 값을 3배로/triple-the-values-of-the-array.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+ublic class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                int num = sc.nextInt();
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
