3d494a4973ece6cd46795e8e23086d9abe369ca6
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-24 14:42:18 +0900

    Title: 배열의 합, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250324/배열의 합/sum-of-array.java
+++ 250324/배열의 합/sum-of-array.java
@@ -6,8 +6,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         for (int i = 0; i < 4; i++){
-            for (int j = 0; j < 4; j++){
-                int total = 0;
+            int total = 0;
+            for (int j = 0; j < 4; j++){                
                 int num = sc.nextInt();
                 total += num;
                 System.out.print(total);
