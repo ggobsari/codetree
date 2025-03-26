c27ec1fe6577246196b6dfcff751d5275a6b39aa
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-26 14:26:36 +0900

    Title: 변수 선언하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250326/변수 선언하기/declaring-variables.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 3;
+        char b = 'C';
+
+        System.out.print(a\n);
+        System.out.print(b);
+    }
+}
