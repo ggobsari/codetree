1fd86a9dfe52f5f236036e776e27446f5feaa89b
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-26 13:41:06 +0900

    Title: 따옴표 출력, Time: 85ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250326/따옴표 출력/print-quote.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        System.out.print("He syas \"It's a really simple sentence\".");
+    }
+}
