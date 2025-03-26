f08d16df5e915a2f2ceed47a8d79b48794110bd9
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-03-26 14:51:49 +0900

    Title: 정수 복사, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

--- 250326/정수 복사/copy-integer.java
+++ 250326/정수 복사/copy-integer.java
@@ -3,7 +3,7 @@ public class Main {
         // Please write your code here.
         int a = 3;
         int b = 4;
-        a = b;
+        b = a;
         System.out.println(a + " " + b);
         System.out.print(a * b);
     }
