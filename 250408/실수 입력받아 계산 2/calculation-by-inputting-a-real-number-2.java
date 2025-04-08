b3d6d2dbf3d7db4c300207c5a367c010051c9392
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-04-08 13:13:24 +0900

    Title: 실수 입력받아 계산 2, Time: 152ms, Memory: 11MB, Status: Wrong Answer - Codetree

---
+++ 250408/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+
+        double result = a * 1.5;
+
+        System.out.printf("%.2f", result);
+    }
+}
