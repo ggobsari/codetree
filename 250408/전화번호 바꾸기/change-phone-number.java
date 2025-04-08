d23bf39535b48398ab99a90517e8a7874cf473a7
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-04-08 13:44:19 +0900

    Title: 전화번호 바꾸기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250408/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String c = sc.next();
+
+        String[] str = c.split("-");
+
+        System.out.print(str[0] + "-" + str[2] + "-" + str[1]);
+    }
+}
