73aec67c5c8fc33dddf74e29efb72000080143a8
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-04-23 06:19:42 +0900

    Title: 2 또는 3의 배수, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 250423/2 또는 3의 배수/multiple-of-2-or-3.java
+++ 250423/2 또는 3의 배수/multiple-of-2-or-3.java
@@ -8,7 +8,7 @@ public class Main {
         int n = sc.nextInt();
         
         for (int i = 1; i <= n; i++){
-            if (i % 2 == 0 && i % 3 == 0){
+            if (i % 2 == 0 || i % 3 == 0){
                 System.out.print(1 + " ");
             } else {
                 System.out.print(0 + " ");
