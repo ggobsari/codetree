e69aa15c22f8c57e980d3d837298045190a0bf0c
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-04-23 05:55:43 +0900

    Title: 숫자의 증감, Time: 139ms, Memory: 12MB, Status: Wrong Answer - Codetree

--- 250423/숫자의 증감/increasing-and-decreasing-numbers.java
+++ 250423/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -12,7 +12,7 @@ public class Main {
             for (int i = 1; i <= n; i++){
                 System.out.print(i + " ");
             }
-        } else {
+        } else if (c.equals('D')){
             for (int i = n; i >=1; i--){
                 System.out.print(i + " ");
             }
