ef62a171b9994efc161e18cf7627411a4938fa43
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-04-23 06:17:44 +0900

    Title: 자연수면 출력, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

--- 250423/자연수면 출력/output-only-natural-number.java
+++ 250423/자연수면 출력/output-only-natural-number.java
@@ -12,6 +12,8 @@ public class Main {
             for (int i = 0; i < b; i++){
                 System.out.print(a);
             }
+        } else {
+            System.out.print(0);
         }
         
     }
