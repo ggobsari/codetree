e2714f487d7167eed94a996d4d7129a0e2742c5c
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-04-22 13:49:44 +0900

    Title: 증가시키며 출력하기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250422/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        int i = 10;
+
+        while(i <= n){
+            System.out.print(i + " ");
+            i += 2;
+        }
+    }
+}
