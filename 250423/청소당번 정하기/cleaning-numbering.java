0f418ebe27fecfc27ac7ac5de0a9aa52983e6ba1
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-04-23 06:50:09 +0900

    Title: 청소당번 정하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250423/청소당번 정하기/cleaning-numbering.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int classroom = 0;
+        int corridor = 0;
+        int toilet = 0;
+
+        for (int i = n; i >= 0; i--){
+            if (n % 12 == 0){
+                toilet++;
+            } else if (n % 3 == 0){
+                corridor++;
+            } else if (n % 2 == 0){
+                classroom++;
+            }
+        }
+
+        System.out.print(classroom + " " + corridor + " " + toilet);
+
+
+    }
+}
