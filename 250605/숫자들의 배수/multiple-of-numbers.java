754379c568108060394ae163ef3a48efd9f56e94
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-06-05 19:50:35 +0900

    Title: 숫자들의 배수, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250605/숫자들의 배수/multiple-of-numbers.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+        int cnt = 1;
+
+        int fiveMulti = 0;
+
+        while (true){
+            n *= cnt;
+            cnt++;
+
+            System.out.print(n + " ");
+
+            if (n % 5 == 0){
+                fiveMulti++;
+                if (fiveMulti == 2){
+                    break;
+                }
+            }
+
+        }
+    }
+}
