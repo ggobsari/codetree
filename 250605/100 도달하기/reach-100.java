0253a03afacbe5c3c0605847fd8855331e2ae1e2
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-06-05 20:05:19 +0900

    Title: 100 도달하기, Time: 137ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250605/100 도달하기/reach-100.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+        int i1 = 1;
+        int i2 = n;
+
+        while (true){
+            int nextIndex = i1 + i2;
+
+            System.out.print(nextIndex + " ");
+
+            if (nextIndex > 100){
+                break;
+            }
+
+            i1 = i2;
+            i2 = nextIndex;
+        }
+
+    }
+}
