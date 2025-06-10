c400519eee0d225deee08201d5fd81545c46630d
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-06-10 12:35:10 +0900

    Title: 특정 위치의 문자, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250610/특정 위치의 문자/char-in-specific-location.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        char[] c = {'L', 'E', 'B', 'R', 'O', 'S'};
+
+        int idx = -1;
+
+        String s = sc.next();
+
+        for (int i = 0; i < c.length; i++){
+            if (c[i].eqauls(s)){
+                idx = i;
+                break;
+            }
+        }
+
+        if (idx != -1){
+            System.out.print(idx);
+        } else {
+            System.out.print("None");
+        }
+    }
+}
