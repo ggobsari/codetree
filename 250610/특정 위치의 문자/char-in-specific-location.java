4f9d84a6a2c3c364a19562b7810e9b98aab5c799
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-06-10 12:39:31 +0900

    Title: 특정 위치의 문자, Time: 116ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250610/특정 위치의 문자/char-in-specific-location.java
+++ 250610/특정 위치의 문자/char-in-specific-location.java
@@ -11,17 +11,17 @@ public class Main {
 
         String s = sc.next();
 
-        for (int i = 0; i < c.length; i++){
-            if (s.eqauls(c[i])){
-                idx = i;
-                break;
-            }
-        }
-
-        // if (idx != -1){
-        //     System.out.print(idx);
-        // } else {
-        //     System.out.print("None");
+        // for (int i = 0; i < c.length; i++){
+        //     if (s.eqauls(c[i])){
+        //         idx = i;
+        //         break;
+        //     }
         // }
+
+        if (idx != -1){
+            System.out.print(idx);
+        } else {
+            System.out.print("None");
+        }
     }
 }
