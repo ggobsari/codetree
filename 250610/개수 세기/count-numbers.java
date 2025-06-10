edb6a0ac2f8d018a0057eb6ea68c4bb800c92a7c
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-06-10 12:47:24 +0900

    Title: 개수 세기, Time: 142ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250610/개수 세기/count-numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++){
+            int num = sc.nextInt();
+
+            if (num == m){
+                cnt++;
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}
