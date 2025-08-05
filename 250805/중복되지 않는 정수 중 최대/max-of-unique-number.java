bab75d7558b25c049f6f29d52b81e7aae347b735
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-08-05 15:53:12 +0900

    Title: 중복되지 않는 정수 중 최대, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250805/중복되지 않는 정수 중 최대/max-of-unique-number.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++){
+            int num = sc.nextInt();
+
+            list.add(num);
+        }
+
+        Collections.sort(list);
+
+        for (int i = 0; i < sort.size(); i++){
+            
+        }
+    }
+}
