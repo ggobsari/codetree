5d7e9c971cc9681a9acfe75ba4f45771c0225c38
ggobsari <162395763+ggobsari@users.noreply.github.com>
2025-08-05 13:43:53 +0900

    Title: 999 또는 -999, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250805/999 또는 -999/999-or-999.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Integer>() arr = new ArrayList<>();
+
+        while (true){
+            int num = sc.nextInt();
+
+            if (num == 999 || num == -999){
+                break;
+            }
+
+            arr.add(num);
+        }
+
+        arr.sort(Comparator.naturalOrder());
+
+        System.out.println(arr.get(0) + " " + arr.get(arr.size() - 1));
+    }
+}
